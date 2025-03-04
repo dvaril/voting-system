1474d61d315b4f52dde7a94957636b27a39762e8
dvaril <semdavesalt@outlook.cz>
2025-03-04 19:07:12 +0100

    fix: fix filament route not found error

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,4 @@ declare(strict_types=1);
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn() => redirect('/admin/login'));
-Route::get('/admin', fn() => redirect('/admin/login'));
 Route::get('/login', fn() => redirect('/admin/login'));
