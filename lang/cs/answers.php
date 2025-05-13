77eeafecb18e0efbe3f1e261caecafd932fe98a6
dvaril <semdavesalt@outlook.cz>
2025-05-13 11:39:27 +0200

    fix: fix typo in cs translation

--- lang/cs/answers.php
+++ lang/cs/answers.php
@@ -150,7 +150,7 @@ return [
 
                     'modal-heading' => 'Odstraňujete odpovědi',
 
-                    'modal-description' => 'Opravdu chcete odstranit tuto odpovědi?'
+                    'modal-description' => 'Opravdu chcete odstranit tyto odpovědi?'
 
                 ]
 
